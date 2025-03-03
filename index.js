d68b1fa7fae28aff78937ea519540bc8045bd100
cocopou <fikri41153.pesonaxii@gmail.com>
2025-03-03 23:12:15 +0700

    feat:update server

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
+const cors = require("cors");
 require("dotenv").config();
 const { products, getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('./products');
 const { users, getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('./users');
 const transporter = require('./email');
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 
 // Middleware untuk validasi input produk
 function validateProduct(req, res, next) {
